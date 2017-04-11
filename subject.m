df4a6c212042299ef4d2d0def1c521879f2c7f0d
kkarbas <kaveh.karbasi@emory.edu>
2017-04-11 16:43:50 -0400

    Initial commit

---
+++ subject.m
@@ -0,0 +1,36 @@
+classdef subject < handle
+    
+    properties
+        experiment
+        session
+        data_path
+    end
+    properties
+        all_events
+        contacts
+        import_type
+        localization
+        math_events
+        montage
+        original_experiment
+        original_session
+        pairs
+        subject_alias
+        task_events
+    end
+    
+    properties(Dependent)
+        
+        
+    end
+    
+    methods
+        
+        function r = subject(varargin)
+            
+        end
+        
+        
+        
+    end
+end
