141c63ec5be5f4eccf20da78b210dc8a46d2a937
kkarbas <kaveh.karbasi@emory.edu>
2017-04-19 12:05:09 -0400

    added getwordlist and seteegrange to FR1 class

--- FR1.m
+++ FR1.m
@@ -1,11 +1,33 @@
 classdef FR1 < experiment
     properties
-
+        noffset
+        poffset
+        buffer
     end
     
     methods
         function obj = FR1(BASE_DIR , expInfo)
             obj@experiment(BASE_DIR , expInfo);
+        end
+        
+    end
+    
+    methods
+        function wList = getwordlist(obj , sessionID) 
+            % get a list of used words in session sessionID of this
+            % experiment
+            wList = obj.sessions(sessionID).geteventfieldvalues('word');
+        end
+        
+        function seteegrange(obj , range , buffer)
+            % set epoch range and buffer for events' eeg data
+            if isequal(size(range) , [1,2]) 
+                obj.noffset = range(1);
+                obj.noffset = range(2);
+                obj.buffer = buffer;
+            else
+                error('Size of first argument should be (1,2)');
+            end
         end
         
     end
