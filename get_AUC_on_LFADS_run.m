7b2f5acb68b61625767fa4a1f0077c367db2f8a7
kkarbasi [Admin (at) SNEL] <kkarbasi@cortex.bme.emory.edu>
2017-07-31 11:59:47 -0400

    added loading and training script to get_AUC_on_LFADS_run.m

--- get_AUC_on_LFADS_run.m
+++ get_AUC_on_LFADS_run.m
@@ -2,7 +2,7 @@
 
 % Load Runs
 % 
-runPath = '/home/mreza/snel/share/derived/lfads_runs/runs/RAM_test/170728_1532_RAMtest';
+runPath = '/home/kkarbasi/snel/share/derived/lfads_runs/runs/RAM_test/170728_1532_RAMtest';
 list_runs = '*';
 
 clear allRuns;
@@ -53,15 +53,65 @@ s1.loadexperiment('FR1');
 
 %%
 % wordEventsEEG = zeros(300,2800,110);
+irun = 1;
+seq = allRuns(irun).sequenceData;
+seq = seq{1};
+Fs = seq(1).fs;
+
+freq_target = 500; %Hz
+
+
+
 for is = 1:numel(seq)
-    tmp = resample(seq(is).rates' , 5 , 3);
-    wordEventsEEG(is , : , :) = tmp(: , 1:110);
+    tmp = resample(seq(is).rates' , freq_target , Fs);
+    numCh = size(tmp , 2)/2;
+    wordEventsEEG(is , : , :) = tmp(: , 1:numCH);
 end
 
 %% replace session event data with loaded event data from LFADS
 s1.experiments.FR1.sessions.x0x30_.wordEventsEEG = wordEventsEEG;
+%% Prepare training data
+disp('wavelength transform')
+expTypes = fieldnames(s1.experiments);
+for iexp = 1:numel(expTypes)
+    
+    curr_exp = expTypes{iexp};
+    disp(['Processing experiment ' curr_exp]);
+    sessIDs = fieldnames(s1.experiments.(curr_exp).sessions);
+    
+    for isess = 1:1%numel(sessIDs)
+        if ~(strcmp(curr_exp , 'catFR1') && isess > 2)
+        curr_sess = sessIDs{isess};
+        disp(['Processing session ' curr_sess]);
+        s1.experiments.(curr_exp).preptrainingdata_from_LFADS(curr_sess , [noffset , poffset] , buffer , [1 ,200] , 50 , resamplef , trim);
 
+        end
+    end
+end
 
+%% Add sessions to sessionCollection (FR1)
+sc = sessionCollection;
+
+FR1_sessions = s1.experiments.FR1.sessions;
+sfn = fieldnames(FR1_sessions);
+
+for i = 1:numel(sfn)
+    sc.addsessionobject(FR1_sessions.(sfn{i}));
+end
+
+%% Add sessions to sessionCollection (catFR1)
+catFR1_sessions = s1.experiments.catFR1.sessions;
+sfn = fieldnames(catFR1_sessions);
+
+for i = 1:2
+    sc.addsessionobject(catFR1_sessions.(sfn{i}));
+end
+
+%% in-session Log Reg
+lambdas = logspace(-7,4,30);
+% lambdas = lambdas(10:end);
+% lambdas = lambdas(16);
+[y_h , y_test ,AUCs , ws , maxIdx] = sc.trainis(lambdas , 25,12);
 
 
 
